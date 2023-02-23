c12a667ffb5cc7eac9879d161a6fe08647971be5
Deea <deea.emilia@ymail.com>
2023-02-23 14:59:18 +0200

    new icons, better structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast';
-const WEATHER_ICON_PREFIX_URL = 'https://openweathermap.org/img/w';
+const WEATHER_ICON_PREFIX_URL = './assets/weather-icons/';
 const API_KEY = '69518b1f8f16c35f8705550dc4161056';
 // e un best practice sa salvam url-urile catre endpoint-uri in variabile
 // const si cu all caps la nume
@@ -8,16 +8,20 @@ const API_KEY = '69518b1f8f16c35f8705550dc4161056';
 async function handleWeather() {
   const search = document.querySelector('.searchBar');
   const city = search.querySelector('.citySearch').value;
-  const date = new Date().toLocaleTimeString('ro', {
-    hour: '2-digit',
-    minute: '2-digit',
+  const date = new Date()
+  const formatter = new Intl.DateTimeFormat(undefined, {
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
   });
+
+  const formattedDate = formatter.format(date);
 
   const loader = document.querySelector('.loading');
 
   document.querySelector(
     '.cityName',
-  ).innerHTML = `Vremea in ${city.toUpperCase()} la ora ${date}`;
+  ).innerHTML = `Weather in ${city.toUpperCase()} at ${formattedDate}`;
   search.style.top = '0';
 
   loader.classList.add('display');
@@ -74,20 +78,24 @@ function showWeather(weather) {
   const humidity = document.querySelector('.humidity');
   const pressure = document.querySelector('.pressure');
 
-  weatherIcon.src = `${WEATHER_ICON_PREFIX_URL}/${weather.weather[0].icon}.png`;
+  weatherIcon.src = getWeatherIconUrl(weather.weather[0].icon);
   degrees.innerHTML = `${Math.round(weather.main.temp)} &#8451`;
-  feelsLike.innerHTML = `Temperatura resimtita ${Math.round(
+  feelsLike.innerHTML = `Feels like ${Math.round(
     weather.main.feels_like,
   )} &#8451`;
-  tempMin.innerHTML = `Temperatura minima ${Math.round(
+  tempMin.innerHTML = `Minimum temperature ${Math.round(
     weather.main.temp_min,
   )} &#8451`;
-  tempMax.innerHTML = `Temperatura maxima ${Math.round(
+  tempMax.innerHTML = `Maximum temperature ${Math.round(
     weather.main.temp_max,
   )} &#8451`;
-  wind.innerHTML = `Viteza vantului ${weather.wind.speed} m/s`;
-  humidity.innerHTML = `Umiditate ${weather.main.humidity} %`;
-  pressure.innerHTML = `Presiunea atmosferica ${weather.main.pressure} bari`;
+  wind.innerHTML = `Wind speed: ${weather.wind.speed} m/s`;
+  humidity.innerHTML = `Humidity: ${weather.main.humidity} %`;
+  pressure.innerHTML = `Atmospheric pressure: ${weather.main.pressure} bari`;
+}
+
+function getWeatherIconUrl(iconCode) {
+  return `./assets/weather-icons/${iconCode}.png`;
 }
 
 function showForecast(forecast) {
@@ -98,22 +106,20 @@ function showForecast(forecast) {
     const month = f.dt_txt.substring(5, 7);
     const day = f.dt_txt.substring(8, 10);
     const weekDay = new Date(Date.UTC(year, month - 1, day));
-    table += `<div>${weekDay.toLocaleDateString('ro', {
-      weekday: 'long',
-      month: 'long',
+    table += `<div>${weekDay.toLocaleDateString('en', {
+      weekday: 'short',
+      month: 'short',
       day: 'numeric',
     })} - ${hour}</div>`;
-    table += `<div><img src="${WEATHER_ICON_PREFIX_URL}/${f.weather[0].icon}.png" /></div>`;
+    table += `<div><img src="${getWeatherIconUrl(f.weather[0].icon)}" /></div>`;
     table += `<div>${f.weather[0].description}</div>`;
     table += `<div>${Math.round(f.main.temp)} &#8451</div>`;
-    // table += `<div>viteza vantului ${f.wind.speed}</div>`;
   });
-  console.log(table);
   document.querySelector('.forecastTable').innerHTML = table;
 }
 
 function handleError({ cod, message }) {
-  alert(`Ceva nu a mers bine: ${cod} - ${message}`);
+  alert(`Something went wrong: ${cod} - ${message}`);
 }
 
 document.addEventListener('keydown', (event) => {
